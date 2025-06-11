34e217bcbb9b8c4b0f2aab88ce53ea8d5ce3dc83
TsionGetaneh <getanehtsion67@gmail.com>
2025-06-11 19:42:29 +0300

    update dashboard

--- lib/features/dashboard/models/dashboard_model.dart
+++ lib/features/dashboard/models/dashboard_model.dart
@@ -21,4 +21,21 @@ class DashboardStats {
     this.nextShift,
   });
 
-  
+ factory DashboardStats.fromJson(Map<String, dynamic> json) {
+    return DashboardStats(
+      totalSalesItems: json['total_sales_items'] ?? 0,
+      todayRevenue: (json['today_revenue'] ?? 0).toDouble(),
+      weeklyRevenue: (json['weekly_revenue'] ?? 0).toDouble(),
+      monthlyRevenue: (json['monthly_revenue'] ?? 0).toDouble(),
+      todayOrders: json['today_orders'] ?? 0,
+      weeklyOrders: json['weekly_orders'] ?? 0,
+      monthlyOrders: json['monthly_orders'] ?? 0,
+      topSellingItems: (json['top_selling_items'] as List?)
+          ?.map((item) => TopSellingItem.fromJson(item))
+          .toList() ?? [],
+      nextShift: json['next_shift'] != null
+          ? NextShift.fromJson(json['next_shift'])
+          : null,
+    );
+  }
+}
