a07820f5646fe07a0961aec3e027d441bc4b4eb4
nuhajoy <nuhaminemnebered@gmail.com>
2025-06-11 20:26:00 +0300

    updated

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:coffee_management/core/theme.dart';
+import 'package:coffee_management/features/auth/providers/auth_provider.dart';
+import 'package:coffee_management/features/auth/models/user_model.dart';
+import 'package:coffee_management/shared/widgets/custom_text_field.dart';
+import 'package:coffee_management/shared/widgets/custom_button.dart';
+import 'package:coffee_management/shared/utils/validators.dart';
+
+class LoginScreen extends ConsumerStatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  ConsumerState<LoginScreen> createState() => _LoginScreenState();
+}
