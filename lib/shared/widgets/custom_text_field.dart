ec3be09a60f9b72786465046640e6bbc697bbd11
Feven777 <fevhai749@gmail.com>
2025-06-11 23:03:41 +0300

    feat: finish up text-field

--- lib/shared/widgets/custom_text_field.dart
+++ lib/shared/widgets/custom_text_field.dart
@@ -28,3 +28,56 @@ class CustomTextField extends StatelessWidget {
     this.maxLines = 1,
     this.enabled = true,
   });
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      validator: validator,
+      onChanged: onChanged,
+      maxLines: maxLines,
+      enabled: enabled,
+      style: const TextStyle(
+        fontSize: 16,
+        color: Colors.white,
+      ),
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        prefixIcon: prefixIcon != null
+            ? Icon(prefixIcon, color: Colors.white70)
+            : null,
+        suffixIcon: suffixIcon,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Colors.white30),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: Colors.white30),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: AppTheme.lightBrown, width: 2),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: AppTheme.error),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(color: AppTheme.error, width: 2),
+        ),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.1),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        labelStyle: const TextStyle(color: Colors.white70),
+        hintStyle: const TextStyle(color: Colors.white54),
+        errorStyle: const TextStyle(color: AppTheme.error),
+      ),
+    );
+  }
+}
