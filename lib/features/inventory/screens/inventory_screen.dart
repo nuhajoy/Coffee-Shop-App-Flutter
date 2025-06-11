e17e664a29034d86ee512102b41912944485cd1e
Eyerusalem Moges Kumsa <jerusalemmoges1111@gmail.com>
2025-06-11 22:24:41 +0300

    inventory6

--- lib/features/inventory/screens/inventory_screen.dart
+++ lib/features/inventory/screens/inventory_screen.dart
@@ -10,3 +10,79 @@ final _inventoryServiceProvider = Provider<InventoryService>((ref) => InventoryS
 final _inventoryItemsProvider = StateProvider<List<InventoryItem>>((ref) => []);
 final _inventoryLoadingProvider = StateProvider<bool>((ref) => false);
 final _inventoryErrorProvider = StateProvider<String?>((ref) => null);
+class InventoryScreen extends ConsumerStatefulWidget {
+  const InventoryScreen({super.key});
+
+  @override
+  ConsumerState<InventoryScreen> createState() => _InventoryScreenState();
+}
+
+class _InventoryScreenState extends ConsumerState<InventoryScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _loadItems();
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _loadItems() async {
+    try {
+      ref.read(_inventoryLoadingProvider.notifier).state = true;
+      ref.read(_inventoryErrorProvider.notifier).state = null;
+
+      final service = ref.read(_inventoryServiceProvider);
+      final items = await service.getAllItems();
+
+      ref.read(_inventoryItemsProvider.notifier).state = items;
+      ref.read(_inventoryLoadingProvider.notifier).state = false;
+    } catch (e) {
+      ref.read(_inventoryLoadingProvider.notifier).state = false;
+      ref.read(_inventoryErrorProvider.notifier).state = e.toString();
+    }
+  }
+
+  Future<void> _addItem(InventoryItem item) async {
+    try {
+      ref.read(_inventoryLoadingProvider.notifier).state = true;
+      ref.read(_inventoryErrorProvider.notifier).state = null;
+
+      final service = ref.read(_inventoryServiceProvider);
+      final newItem = await service.addItem(item);
+
+      final currentItems = ref.read(_inventoryItemsProvider);
+      ref.read(_inventoryItemsProvider.notifier).state = [...currentItems, newItem];
+
+      ref.read(_inventoryLoadingProvider.notifier).state = false;
+    } catch (e) {
+      ref.read(_inventoryLoadingProvider.notifier).state = false;
+      ref.read(_inventoryErrorProvider.notifier).state = e.toString();
+    }
+  }
+
+  Future<void> _deleteItem(String id) async {
+    try {
+      ref.read(_inventoryLoadingProvider.notifier).state = true;
+      ref.read(_inventoryErrorProvider.notifier).state = null;
+
+      final service = ref.read(_inventoryServiceProvider);
+      await service.deleteItem(id);
+
+      final currentItems = ref.read(_inventoryItemsProvider);
+      ref.read(_inventoryItemsProvider.notifier).state =
+          currentItems.where((item) => item.id != id).toList();
+
+      ref.read(_inventoryLoadingProvider.notifier).state = false;
+    } catch (e) {
+      ref.read(_inventoryLoadingProvider.notifier).state = false;
+      ref.read(_inventoryErrorProvider.notifier).state = e.toString();
+    }
+  }
