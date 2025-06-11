49822e73f82c4446ee4c6dd9bbc48386891261cf
Bitanya Aleme <bitanyaaleme29@gmail.com>
2025-06-11 20:56:46 +0300

    shift_4

---
+++ lib/features/shifts/providers/shift_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:coffee_management/features/shifts/models/shift_model.dart';
+import 'package:coffee_management/features/shifts/services/shift_service.dart';
+import 'package:coffee_management/features/auth/models/user_model.dart' as auth_models;
+
+final shiftServiceProvider = Provider<ShiftService>((ref) => ShiftService());
+
+// All shifts (admin only)
+final shiftsProvider = FutureProvider<List<Shift>>((ref) {
+  final service = ref.watch(shiftServiceProvider);
+  return service.getAllShifts();
+});
